8f1fda6521ae20dbe274de5648b2117ee08e971d
Noah Hallows <noahhallows@gmail.com>
2023-11-04 22:27:54 +1100

    I am the Laughing man!

--- main_2.py
+++ main_2.py
@@ -3,19 +3,31 @@ import os
 import cv2 as cv
 import sys
 
-# Assuming you would replace these with the user input or environment variables
+
 face_to_search_for_location = "/home/noah/Documents/software_mp/test data/img2.jpg"
 images_to_search_location = "/home/noah/Documents/software_mp/test data/"
 
+#ask user what they want to do to matching images
+action = input("what do you want to do to matching images? 1: blur, 2 replace face: ")
+try:
+    action = int(action)
+except:
+    print("enter a valid number")
+    exit()
 # Load the image containing the face you want to look for
 face_to_search_for = face_recognition.load_image_file(face_to_search_for_location)
 face_to_search_for_encoding = face_recognition.face_encodings(face_to_search_for)[0]
+
+#load overlay image
+overlay_location = "/home/noah/Documents/software_mp/Laughing_man.png"
+
+
 
 # Lists to store the results
 images_that_match = []
 images_that_do_not_match = []
 
-def blur(image_bgr, target_face_location, image_location):
+def blur(image_bgr, target_face_location):
     # Unpack the location
     top, right, bottom, left = target_face_location
 
@@ -29,17 +41,48 @@ def blur(image_bgr, target_face_location, image_location):
     face_roi = image_bgr[top:bottom, left:right]
 
     # Apply a Gaussian blur to the face region
-    blurred_face = cv.GaussianBlur(face_roi, (99, 99), 30)
+    blurred_face = cv.GaussianBlur(face_roi, (199, 199), 0)
 
     # Replace the original image region with the blurred face
     image_bgr[top:bottom, left:right] = blurred_face
 
-    # Save the final image
-    cv.imwrite(image_location, image_bgr)
+    return image_bgr
+
+def replace(background, overlay, target_face_location):
+    # Unpack the location
+    top, right, bottom, left = target_face_location
+    # Calculate the width and height of the bounding box
+    width = right - left
+    height = bottom - top
+
+    # Resize overlay image to match the bounding box size
+    overlay_resized = cv.resize(overlay, (width, height))
+
+    # Check if overlay image has an alpha channel (transparency)
+    if overlay_resized.shape[2] == 4:
+        # Split overlay into color and alpha channels
+        overlay_color = overlay_resized[:, :, :3]
+        alpha_mask = overlay_resized[:, :, 3] / 255.0
+
+        # Get the ROI from the background and blend using the alpha mask
+        roi = background[top:bottom, left:right]
+        roi = cv.addWeighted(overlay_color, alpha_mask, roi, 1 - alpha_mask, 0, roi)
+
+        # Put the blended ROI back into the background
+        background[top:bottom, left:right] = roi
+    else:
+        # If no alpha channel, just replace the ROI with the resized overlay
+        background[top:bottom, left:right] = overlay_resized
+
+    return background
+
+
 
 def face_recog():
     # Get list of all files in the directory specified
     images_to_search = os.listdir(images_to_search_location)
+    if action == 2:
+        overlay = cv.imread(overlay_location)
 
     for image_name in images_to_search:
         current_image_to_search_location = os.path.join(images_to_search_location, image_name)
@@ -68,7 +111,12 @@ def face_recog():
                         match = face_recognition.compare_faces([face_to_search_for_encoding], target_face_encoding)
                         # If it's a match, blur the face
                         if match[0]:
-                            blur(image_bgr, target_face_location, current_image_to_search_location)
+                            if action == 1:
+                                new_image = blur(image_bgr, target_face_location)
+                            elif action == 2:
+                                new_image = replace(image_bgr, overlay, target_face_location)
+                            cv.imwrite(current_image_to_search_location, new_image)
+
                 else:
                     print(f"Image {image_name} doesn't match")
                     images_that_do_not_match.append(image_name)
