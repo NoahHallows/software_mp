eb1e26295e8a119700fb8653f9019950ef16aef2
Noah Hallows <noahhallows@gmail.com>
2023-11-05 09:20:30 +1100

    started on video

--- main_2.py
+++ main_2.py
@@ -142,7 +142,27 @@ def face_recog(image_name):
     except Exception as e:
         print(f"An error occurred with image {image_name}: {e}")
 
-    
+def video():
+    cap = cv.VideoCapture(0)
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        # if frame is read correctly ret is True
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+        # Our operations on the frame come here
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        # Display the resulting frame
+        cv.imshow('frame', gray)
+        if cv.waitKey(1) == ord('q'):
+            break
+    # When everything done, release the capture
+    cap.release()
+    cv.destroyAllWindows()    
 
 def display_image(images_that_match):
     for image_name in images_that_match:
