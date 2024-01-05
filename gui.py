9f1f85c1802d578ce95ce214546551193b01689f
Noah Hallows <noahhallows@gmail.com>
2024-01-05 15:36:57 +1100

    dfghdfh

--- gui.py
+++ gui.py
@@ -11,18 +11,17 @@ file_list_column = [
     ],
     [
         sg.Listbox(
-            values=[], enable_events=True, size=(40, 20), key="-KET LIST-"
+            values=[], enable_events=True, size=(40, 20), key="-FILE LIST-"
         )
     ],
 ]
 
 
-
 # For now will only show the name of the file that was chosen
 image_viewer_column = [
-    [sg.Text("Choose an image from the list on the left:")]
-    [sg.Text(size=(40, 1), key="-TOUT-")]
-    [sg.Image(key="-IMAGE")]
+    [sg.Text("Choose an image from list on left:")],
+    [sg.Text(size=(40, 1), key="-TOUT-")],
+    [sg.Image(key="-IMAGE-")],
 ]
 
 # ----- Full layout -----
@@ -36,31 +35,39 @@ layout = [
 
 window = sg.Window("Image Viewer", layout)
 
+# Run the Event Loop
 while True:
-    event, values = window.Read()
+    event, values = window.read()
     if event == "Exit" or event == sg.WIN_CLOSED:
         break
 
     # Folder name was filled in, make a list of files in the folder
     if event == "-FOLDER-":
-        folder = values["-FOLDER"]
+
+        folder = values["-FOLDER-"]
+
         try:
             # Get list of files in folder
             file_list = os.listdir(folder)
         except:
             file_list = []
-        
+
         fnames = [
             f
             for f in file_list
             if os.path.isfile(os.path.join(folder, f))
             and f.lower().endswith((".png", ".gif"))
         ]
-        window["-FILES LIST-"].update(fnames)
-    elif event == "-FILE LIST-":
+
+        window["-FILE LIST-"].update(fnames)
+
+    elif event == "-FILE LIST-":  # A file was chosen from the listbox
+
         try:
             filename = os.path.join(
-                values["-FOLDER"], values["-FILE LIST-"][0]
+
+                values["-FOLDER-"], values["-FILE LIST-"][0]
+
             )
             window["-TOUT-"].update(filename)
             window["-IMAGE-"].update(filename=filename)
