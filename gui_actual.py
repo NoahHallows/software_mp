2ae302d904ab7a57a5688e5ac6fde5599805c5b6
Noah Hallows <noahhallows@gmail.com>
2024-01-06 19:26:34 +1100

    I am having difficulty with the progress bar

--- gui_actual.py
+++ gui_actual.py
@@ -5,6 +5,8 @@ import face_recognition
 #import face_id_picture
 import editing_image
 import multiprocessing
+import threading
+
 
 
 radio_keys = ("-R1-", "-R2-")
@@ -14,6 +16,11 @@ radio_unchecked = b'iVBORw0KGgoAAAANSUhEUgAAABkAAAAZCAYAAADE6YVjAAAEwElEQVR4nI1W
 # Base64 Encoded Radio Button Image of checked radio button
 radio_checked = b'iVBORw0KGgoAAAANSUhEUgAAABkAAAAZCAYAAADE6YVjAAAF40lEQVR4nI2Wf2yWVxXHv+fe+7y/3xbYWvpzhbGRCOkMLoRsjr21A2dI2BalTeaYxsyQ6GT+YTQuQRsy4zRGtmg2gzGNf+jinoK6sY2ZbNK3JQuSuWmiWx3ggBQKfTta+v58nueee/zjfQusMPD88yT3ued87sk593sPcCMTUblDYgZ80R9b90XnDomBiLphjOsEp8WBNQEiohUt2uuLhsji1Ut2zR8Dvq9HBgcZAPqPzK+ZD81DxWpwt2XucYIURCqa6FQmHnuryeBPY31N79dhvkbD77qQAV/0yCBx7tBMV0knn5oPooczyVR8Rcyi0zAS5FBhYDLQ+DDUKJWrtaxRf0hF87uObL3lzIL/J0IWNmx8c7Z/zsR/b7Rp25qex7aOuL09ayhhiECAs4xSyPLBxVD2T4bmQLkZURRNZaLi9nce7P4rfNG4AnQZIqJA5O4Zu5Cbk+TrHVRL/Hi1ie5cnjBgosAyWAAnAnEOEIcYCbRjOXy+an94XHlTHK8tcZUvvP1AR34h3mXIUL1DNm2eaTsXxN5t96R1uNdw15KkrgQMAqAgEAAiAuccnHOI2MFah4wWHJ+t8OMTWp8L9fn2uKwbP9JyHgCwm5wCgIG1IOwmdyH0no4lkq0/uQ22qzmhyzWGIUARINfqEBF4GrBaY83NKb2rJ7Amnlg+U+GnsZvcwNoRqmfSSOu+sYurT1Xdv7a3Oj10R5bKoZAhwAlAtBBTLmViLcMoQhBZfH84j7vXduLhDT3yvX+U5Y8fJXlVMlo7trX7GIZEqdwoFADMMn0pm057X2w3zjkQpH76mFFwTi4BRASWHYxWYCfY+dwb+M3L7+Bn/lHMViN6YDlcOpnwpgO1DQByfVAqXxgRACgHduMKz2JVxlBgHTxNIABnZopIJQwsuwaAYTTBOYcdzx7Ei2MT6O5Yih999bOA1rglAer2IpQZ9wBAvjAiCoODLCJkWXo6TIS4EoqsAwB899dv4q4nfouxf55GNh1HLYhgVD2zHc++jn2HP0D7sjR++c1+3PfpbhSrIZIa1KZCWJYVIkIYHOQF3dFOJJWAA4mAnQOzxdRHRZwtFPGVn76MN94+gZuWphBGFjueOYiR8f+gY1kGzz++CZ+7owuFi5X6nRBBHAxxkhodhQYA04AwQSoVJkTMcE7BMjD8nS0gIuwbn8BjP38Nz+3cjJH8BF7MT6Dz5gye37kJud5OFObKUASwc4gco+o8CFDp6wPXIb6viYhXv3rh5GSkP1UKQ1EaCEJG3NPY++374UTw0lvH8PU9B1GuRWi/KYNffWsz+no7MT1XgSLUa+YcSiHLmcgTD+FJIhL4vla5lgECgFQM4ycDQ8fmI/EgcCKoBhEIgr1PfB4P3nUbpueqaE7HsbeRwfRcGYoEzK7eEMI4XmSZjGKU8PQYAORaBsjkR+EAoNmofadL5d37zrLpbYoktEQeESq1EDFP4xff6Ec26WHL+pVXANAAOITWIUaRvFrQqlyphh0x3g8A+VE4ulIYe18pDLtE+mt72gt2Q0vCzIYCTwHOCYgIqbhBEFlUamG9kA15qVlGRjkcLQR21/kuo2rl4ROPdD+GAV9jZJA/pl259dOtU2LebTW27Zlbq7yyKabnQqnfTAiY619qACzX9SujGP+9GPCTp5bogjXnsiZc996/V0wvaNdVKvyZA2c2zqv0X1pRSz7ZVYnWL9UmFKKABdbVayUigGMYOChn5egM2z3nmr2CJCtZW73/vUd6Dl+twgvWeAfW/fn0vSXd9DttdHe/nsaWFmdXJkEJJUQQROxQDllOlEVeK2gzatvAbE+ng+L29x9dNf7J70nDFupz5/6T7dVY9qli6L6ciMWSXSZAOwWIE6PKhLM2jknroVwNqxmPXlgSXPjB3x9dM7UYcE1IPaPLb/WGA9O3zzM9VAr5XhvZlQ6SIaGSUfRh0jP5ZRS+9Ldt3ccW+/1/JkJYNK0oAg6JmKtmIN+/7rRyYxuqz12LgfD9+tw1dOO563+8H1VJkK2keQAAAABJRU5ErkJggg=='
 
+# Global variable or event to signal start
+start_processing = threading.Event()
+
+# Initialize a multiprocessing Queue for progress updates
+progress_queue = multiprocessing.Queue()
 
 #Theme
 sg.theme("DarkGrey6")
@@ -72,16 +79,30 @@ options = [
 
 #Progress bar
 progress_bar = [
-    [sg.Text('Progress')],
+    [sg.Text('Progress', visible=False, k="PROGRESS_TEXT")],
     [
-        sg.ProgressBar(100, orientation='h', size=(20, 20), key='progressbar', visible=True, expand_x=True, bar_color="green on silver"),
+        sg.ProgressBar(100, orientation='h', size=(20, 20), key='progressbar', visible=False, expand_x=True, bar_color="green on silver"),
     ],
 ]
 
+#list of matching images
+matching_image_list = [
+    [sg.Text("Images that match:")],
+    [
+        sg.Listbox(
+            values=["rer"], enable_events=True, select_mode="LISTBOX_SELECT_MODE_MULTIPLE", key="matching_images", size=(40, 20)
+        )
+    ],
+    #sg.Image(key="image_display")
+]
 
+#End layout
+end_layout = [
+    [sg.Column(matching_image_list)]
+]
 
-# Layout
-layout = [
+# Inital Layout
+start_layout = [
     select_target_face,
     select_image_dir,
     [
@@ -95,7 +116,9 @@ layout = [
     [sg.Button('Start', size=(5, 2), button_color=('white', 'springgreen4'), key='SUBMIT')],
 ]
 
-window = sg.Window("Face removal tool", layout)
+print(end_layout)
+
+window = sg.Window("Face removal tool", start_layout)
 
 def get_files(images_to_search_location):
     global images_to_search
@@ -114,6 +137,7 @@ def get_files(images_to_search_location):
     return images_to_search
 
 def face_recog(image_name):
+    print(image_name)
     used_kcf = False
     try:
         # Load and run face recognition on the image to search
@@ -145,20 +169,30 @@ def face_recog(image_name):
                         #cv.imwrite(image_name, new_image)
                     # Put progress update to the queue
                     progress_queue.put(1)
+                    # Get progress update from the queue
+                    processed_images = progress_queue.get()
+                    print(processed_images)
+                    # Calculate progress percentage
+                    progress_percentage = (processed_images / total_images) * 100
+                    print(progress_percentage)
+                    # Update progress bar
+                    window['progressbar'].update_bar(progress_percentage)            
+                    #monitor_progress()
+                    print("updated")
                     return image_name
 
             else:
                 # Put progress update to the queue
-                progress_queue.put(2)
-                return f"Image {image_name} doesn't match"
+                progress_queue.put(1)
+                #return f"Image {image_name} doesn't match"
         else:
             # Put progress update to the queue
-            progress_queue.put(2)
-            return f"No faces found in image {image_name}"
+            progress_queue.put(1)
+            #return f"No faces found in image {image_name}"
 
     except Exception as e:
-        progress_queue.put(3)
-        return f"An error occurred with image {image_name}: {e}"
+        #progress_queue.put(1)
+        print(f"An error occurred with image {image_name}: {e}")
 
 
 def check_radio(key):
@@ -168,23 +202,24 @@ def check_radio(key):
     window[key].update(radio_checked)
     window[key].metadata = True
 
-def monitor_progress(window, total_images):
-    processed_images = 0
-    
-    while processed_images < total_images:
-        try:
-            # Get progress update from the queue
-            progress_queue.get_nowait()
-            processed_images += 1
-            #percentage =+ 1
-            # Calculate progress percentage
-            progress_percentage = (processed_images / total_images) * 100
-            print(progress_percentage)
-            # Update progress bar
-            window['progressbar'].update_bar(progress_percentage)
+processed_images = 0
+
+def monitor_progress():
+    global processed_images
+    print("updating")
+    #while processed_images < total_images:
+    try:
+        # Get progress update from the queue
+        data = progress_queue.get()
+        processed_images += 1
+        # Calculate progress percentage
+        progress_percentage = (processed_images / total_images) * 100
+        print(progress_percentage)
+        # Update progress bar
+        window['progressbar'].update_bar(progress_percentage)
             
-        except:# multiprocessing.Queue.empty:
-            continue
+    except Exception as e:
+        print(f"an error occured {e}")
 
 def start_face_recognition(face_to_search_for_location, action_passed, overlay_location):
     global action, overlay, face_to_search_for_encoding
@@ -195,10 +230,12 @@ def start_face_recognition(face_to_search_for_location, action_passed, overlay_l
     if action == 2:
         #if selected access the overlay image
         overlay = cv.imread(overlay_location)          
-
-    with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
+    #for images in images_to_search:
+    #    results = face_recog(images)
+    with multiprocessing.Pool(processes=(multiprocessing.cpu_count()-1)) as pool:
         # Map the image processing function over the images
         results = pool.map(face_recog, images_to_search)
+    print(results)
     results = [item for item in results if item is not None]
     return results
 
@@ -210,7 +247,15 @@ overlay_location = ''
 #list what elements to disable
 key_list = 'OVERLAY_LOCATION', 'OVERLAY_BROWSER'
 
-
+def end_event_loop(results):
+    window = sg.Window("Face removal tool", end_layout)
+    while True:
+        event, values = window.Read()
+        window["matching_images"].update(results)
+        if event == "Exit" or event == sg.WIN_CLOSED:
+            break
+            
+    
 #Event loop
 while True:
     event, values = window.Read()
@@ -296,12 +341,24 @@ while True:
         images_to_search = get_files(folder)
         # Calculate total number of images
         total_images = len(images_to_search)
-        # Initialize a multiprocessing Queue for progress updates
-        progress_queue = multiprocessing.Queue()
+        #Set these to visible
+        window['progressbar'].update(visible=True)
+        window["PROGRESS_TEXT"].update(visible=True)
         # Start monitoring progress in a separate process
-        multiprocessing.Process(target=monitor_progress, args=(window, total_images)).start()
-        results = start_face_recognition(target_face_location, action, overlay_location)
-        print(results)
+        # Start monitoring progress in a separate thread
+        #monitor_thread = multiprocessing.(target=monitor_progress, args=(window, total_images, progress_queue))
+        #monitor_thread.start()
+            
+        # Signal to start the processing tasks
+        start_processing.set()
 
+        results = start_face_recognition(target_face_location, action, overlay_location)
+        #Popup telling user program is finished
+        sg.popup_notify(title="Finished")
+        print(results)
+        window.Close()
+        break
+
+end_event_loop(results)
 
 window.Close()
