cd919b09a20cfdd89d2742717a56b127c238f844
Noah Hallows <noahhallows@gmail.com>
2024-01-10 16:05:28 +1100

    Got file list working

--- python/gui-qt.py
+++ python/gui-qt.py
@@ -1,9 +1,20 @@
 import sys
-from PySide6.QtWidgets import QApplication, QDialog, QDialogButtonBox, QFormLayout, QLineEdit, QVBoxLayout, QFileDialog, QProgressBar, QRadioButton, QPushButton, QLabel, QGridLayout
+from PySide6.QtWidgets import QApplication, QDialog, QDialogButtonBox, QFormLayout, QLineEdit, QVBoxLayout, QFileDialog, QProgressBar, QRadioButton, QPushButton, QLabel, QGridLayout, QListWidget
 from PySide6.QtCore import Slot
 import re
+import cv2 as cv
+import os
 
 class Window(QDialog):
+    target_face_location = ""
+    images_to_search_location = ""
+    # Logic for getting radio button value
+    @Slot()
+    def radio(self):
+        button = self.sender()
+        action = button.option
+        print(f"action {action} was selected")
+    
     def __init__(self):
         super().__init__(parent=None)
         self.setWindowTitle("Face removal tool")
@@ -23,30 +34,75 @@ class Window(QDialog):
         gridLayout.addWidget(QLabel("Select the directory contining images to search:"), 1, 0)
         gridLayout.addWidget(self.select_image_directory_text_box, 1, 1)
         gridLayout.addWidget(select_image_directory_button, 1, 2)
-
         select_image_directory_button.clicked.connect(self.select_image_directory)
-        
+
+        # List images
+        self.image_list_box = QListWidget()
+        gridLayout.addWidget(self.image_list_box, 2, 0, 1, 2)
+
+        # Select action option
+        blur_button = QRadioButton("Blur matching faces", self)
+        blur_button.option = 1
+        replace_button = QRadioButton("Replace matching faces", self)
+        replace_button.option = 2
+        delete_button = QRadioButton("Delete images containg matching faces", self)
+        delete_button.option = 3
+        # Add radio buttons to layout
+        gridLayout.addWidget(QLabel("What should the program do to matching faces:"), 3, 0, 1, 2)
+        gridLayout.addWidget(blur_button, 4, 0)
+        gridLayout.addWidget(replace_button, 5, 0)
+        gridLayout.addWidget(delete_button, 6, 0)
+        # Connect radio buttons to fuction
+        blur_button.clicked.connect(self.radio)
+        replace_button.clicked.connect(self.radio)
+        delete_button.clicked.connect(self.radio)
+
         # Add formLayout to dialogLayout
         dialogLayout.addLayout(gridLayout)
         # Set dialogLayout to be the layout of the window
         self.setLayout(dialogLayout)
-    
+        
+
+
     # Logic for selecting image directory
     @Slot()
     def select_image_directory(self):
+        global images_to_search_location
         images_to_search_location = QFileDialog.getExistingDirectory(self, ("Open folder"))
         if images_to_search_location:
             self.select_image_directory_text_box.clear()
             self.select_image_directory_text_box.setText(str(images_to_search_location))  #('/home/noah/Documents/software_mp/test data/picture_of_me_at_mdda.jpg', 'folder (*.png *.jpg *.bmp)')
+            # Display images in directory
+            try:
+                # Get list of files in folder
+                file_list = os.listdir(images_to_search_location)
+            except:
+                file_list = []
+
+            fnames = [
+                f
+                for f in file_list
+                if os.path.isfile(os.path.join(images_to_search_location, f))
+                and f.lower().endswith((".png", ".gif", '.jpg'))
+            ]
+            for image in fnames:
+                self.image_list_box.addItem(image)
+
     # Logic for selecting target image
     @Slot()
     def select_target_face(self):
+        global target_face_location
         target_face_location_untrimmed = QFileDialog.getOpenFileName(self, ("Open image"), "", ("folder (*.png *.jpg *.bmp)"))
         if target_face_location_untrimmed:
             self.target_image_text_box.clear()
             # Use regular expression to find the file path
             target_face_location = re.search(r"'(.*?)'", str(target_face_location_untrimmed)).group(1)
             self.target_image_text_box.setText(target_face_location)
+    
+    def display_target_image(self):
+        pass
+
+
 
 
 
